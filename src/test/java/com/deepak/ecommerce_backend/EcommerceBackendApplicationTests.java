ba8574b32b366cc65c86493b32e89f1318f88743
deepak <tiwarideepak569@gmail.com>
2026-07-26 10:14:07 +0530

    feat : initial commit with spring boot setup and base properties

---
+++ src/test/java/com/deepak/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
